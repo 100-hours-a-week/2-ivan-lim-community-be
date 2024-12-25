c39c716d8fd11e85ccce03978da1b6a5878bbfb7
Lim <inlim@Limui-MacBookAir.local>
2024-12-25 14:24:07 +0900

    make week3_server branch

--- week2_server/README.md
+++
@@ -1,3 +0,0 @@
-현 폴더에 위치한 data.json의 users 배열에 저장되어 있는 email, password를 이용해 test 진행.
-ex) email : example@example.com
-    password : Abc1234!
--- week2_server/api/like/likeRouter.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import {  } from './likeController.js';
-
-const likeRouter = express.Router();
-
-
-export default likeRouter;
